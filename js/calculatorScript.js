bf3ecf2c885bf55508a303f412b366b09766333c
Adrian Savarese <as@gmail.com>
2021-03-24 22:12:26 -0300

    Version 2

--- js/calculatorScript.js
+++ js/calculatorScript.js
@@ -7,7 +7,7 @@ let finalOperation = ""
 
 function captureNumber(numero) {
     numberStringOne = numberStringOne + numero
-    numberStringOneToInt = parseInt(numberStringOne)
+    numberStringOneToInt = parseFloat(numberStringOne)
     htmlResult.innerHTML = numberStringOne
 }
 
@@ -15,24 +15,33 @@ function captureNumber(numero) {
 function operationNumber(operationId) {
     
     if (operationId =="d" || operationId =="x" || operationId =="-" || operationId =="+") {
-    numberStringTwoToInt = numberStringOneToInt
-    numberStringOneToInt = 0
-    numberStringOne = ""
-    finalOperation = operationId    
+    numberStringTwoToInt = numberStringOneToInt;
+    numberStringOneToInt = 0;
+    numberStringOne = "";
+    finalOperation = operationId;
+    }
+    if (operationId == "clearAll") {
+        numberStringOneToInt = 0;
+        numberStringTwoToInt = 0;
+        numberStringOne = "";
+        htmlResult.innerHTML = numberStringOneToInt;
     }
     if(operationId =="eqResult") {
             
         if (finalOperation == "+") {
                 numberStringTwoToInt = numberStringTwoToInt + numberStringOneToInt;
-                htmlResult.innerHTML = numberStringTwoToInt
+                htmlResult.innerHTML = numberStringTwoToInt;
             } else if (finalOperation =="-") {
                 numberStringTwoToInt = numberStringTwoToInt - numberStringOneToInt;
-                htmlResult.innerHTML = numberStringTwoToInt
+                htmlResult.innerHTML = numberStringTwoToInt;
             } else if (finalOperation =="x") {
                 numberStringTwoToInt = numberStringTwoToInt * numberStringOneToInt;
-                htmlResult.innerHTML = numberStringTwoToInt
+                htmlResult.innerHTML = numberStringTwoToInt;
+            } else if (finalOperation =="d") {
+                numberStringTwoToInt = numberStringTwoToInt / numberStringOneToInt;
+                htmlResult.innerHTML = numberStringTwoToInt;
             } else {
-                alert("falla"); //ojo que si aprieto igual sin poner ningun numero voy aca
+                alert("Ups, error");
             }
     }       
         
